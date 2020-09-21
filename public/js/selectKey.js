5461cf7dc1f644349715a85ce180ad7d546c20d6
VCarlosJP <carlosjp.villanueva@gmail.com>
2020-09-21 18:15:19 +0200

    SeleckKeys view done

--- public/js/selectKey.js
+++ public/js/selectKey.js
@@ -36,13 +36,23 @@ $("#step4").removeClass( "active");
             table_header.appendChild(document.createTextNode('Columns from '+clicked_table_name));
             
             tables[selected_table].forEach(function(column_name){
-
-                $('#columns-container').append(`<div class="inputGroup""><input id="${column_name}" name="column-from-table" type="checkbox" checked /><label for="${column_name}">${column_name}</label></div>`);
+                if(columns_to_ignore[selected_table].includes(column_name))
+                    $('#columns-container').append(`<div class="inputGroup""><input id="${column_name}" name="column-from-table" type="checkbox" /><label for="${column_name}">${column_name}</label></div>`);
+                else
+                    $('#columns-container').append(`<div class="inputGroup""><input id="${column_name}" name="column-from-table" type="checkbox" checked /><label for="${column_name}">${column_name}</label></div>`);
             
             });
             
-            
+    
             document.getElementById("uncheck-all-columns").disabled = false;
+
+            if(checkbox_status[selected_table]===true){
+                $("#uncheck-all-columns").prop("checked", true);
+                    
+            }
+            else
+                $("#uncheck-all-columns").prop("checked", false);
+            
         }
 
         //If a user clicks on a column checkbox, the function will add the columns name into the
@@ -68,33 +78,72 @@ $("#step4").removeClass( "active");
 
         //Executed when a user clicks on "Uncheck all the columns" checkbox.
         $('#uncheck-all-columns').change(function(event) {
-            uncheckAllColumns(this);
+            
+            if(checkbox_status[selected_table]===false){
+                checkbox_status[selected_table]=true;
+                uncheckAllColumns();
+                addAllColumnsCheckbox();
+            }
+                
+            else{
+                checkbox_status[selected_table]=false;
+                checkAllColumns();
+                removeAllColumnsCheckbox();
+            }
+               
+            
         });
 
         /**
          * This function it is in case a user only wants to keep a few columns. So, clicking the "Uncheck all the columns" checkbox
          * will uncheck all the columns displayed and it will be easier to the user check the ones that are needed.
          * */
-        function uncheckAllColumns(checkbox_element){
-            checkboxes = document.getElementsByName("column-from-table");
-            if(checkboxes.length != 0){
-                if(checkbox_element.checked){
+        // function uncheckAllColumns(checkbox_element){
+        //     checkboxes = document.getElementsByName("column-from-table");
+        //     if(checkboxes.length != 0){
+        //         if(checkbox_element.checked){
                     
-                    checkboxes.forEach(function(checkbox){
-                        checkbox.checked = false;
-                        if(!columns_to_ignore[selected_table].includes(checkbox.id))
-                            columns_to_ignore[selected_table].push(checkbox.id);
-                        });
+        //             checkboxes.forEach(function(checkbox){
+        //                 checkbox.checked = false;
+        //                 if(!columns_to_ignore[selected_table].includes(checkbox.id))
+        //                     columns_to_ignore[selected_table].push(checkbox.id);
+        //                 });
                        
-                }else{
-                    checkboxes.forEach(function(checkbox){
-                        checkbox.checked = true;
-                        const index = columns_to_ignore[selected_table].indexOf(checkbox.id);
-                        columns_to_ignore[selected_table].splice(index, 1);
-                    });
-                }
-            }
+        //         }else{
+        //             checkboxes.forEach(function(checkbox){
+        //                 checkbox.checked = true;
+        //                 const index = columns_to_ignore[selected_table].indexOf(checkbox.id);
+        //                 columns_to_ignore[selected_table].splice(index, 1);
+        //             });
+        //         }
+        //     }
+        // }
+        function uncheckAllColumns(){
+            checkboxes = document.getElementsByName("column-from-table");
+            checkboxes.forEach(function(checkbox){
+                
+                    checkbox.checked = false;
+            });
         }
 
+        function checkAllColumns(){
+            checkboxes = document.getElementsByName("column-from-table");
+            checkboxes.forEach(function(checkbox){
+                checkbox.checked = true;
+            });
+        }
+
+        function addAllColumnsCheckbox(){
+            checkboxes = document.getElementsByName("column-from-table");
+            columns_to_ignore[selected_table] = [];
+            checkboxes.forEach(function(checkbox){
+                columns_to_ignore[selected_table].push(checkbox.id);
+            });
+        }
+
+        function removeAllColumnsCheckbox(){
+            checkboxes = document.getElementsByName("column-from-table");
+            columns_to_ignore[selected_table] = [];
+        }
         
 
